92e1280f5b3643c0d7df8209f6ccc7741aa57598
summer <602165057@qq.com>
2018-09-05 17:44:30 +0800

    xyl

--- routes/oAuthRouter.js
+++ routes/oAuthRouter.js
@@ -1,11 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var OAuthConfig = require('../config/config').OAuthConfig;
-var axios = require('axios');
-const timeout = 30000;
-var log = require('log4js').getLogger("oAuther");
-
-var service = require('../service/loginService');
 
 /* GET home page. */
 router.get('/github', function (req, res, next) {
@@ -18,42 +13,5 @@ router.get('/github', function (req, res, next) {
   //转发到授权服务器
   res.redirect(path);
 });
-router.get("/github_callback", function (req, res, next) {
-  let code = req.query.code;
-  let state = req.query.state;
-  let path = "/login/oauth/access_token";
-  let headers = {};
-  headers.host = 'github.com';
 
-  path += '?client_id=' + OAuthConfig.GITHUB_CLIENT_ID;
-  path += '&client_secret=' + OAuthConfig.GITHUB_CLIENT_SECRET;
-  path += '&code=' + code;
-  try {
-    var reqUrl = `https://github.com${path}`;
-    axios.post(reqUrl, {}, {}).then((response) => {
-      let { data } = response
-      var args = data.split('&');
-      var tokenInfo = args[0].split("=");
-      var token = tokenInfo[1];
-      //利用access_token向资源服务器请求用户授权数据
-      var url = `https://api.github.com/user?access_token=${token}&scope=user`;
-      axios({
-        method: 'get',
-        url: url,
-        timeout: timeout
-      }).then((response) => {
-        let { data } = response;
-        let params = {
-          openId: data.node_id,
-          portrait: data.avatar_url,
-          nikeName: data.name
-        };
-        service.insertOAuthUser(params, res, next);
-      })
-    });
-  } catch (err) {
-    log.error(err);
-    return res.json({ code: 500 })
-  }
-});
 module.exports = router;
--- service/loginService.js
+++ service/loginService.js
@@ -9,12 +9,35 @@ const config = require('../config/config');
 const jwt = require('jsonwebtoken');
 const secret = 'SUMMERNOTE';
 
+/**
+ * 登录成功后返回对象
+ * @param {*} resultMap 
+ * @param {*} userId 
+ * @param {*} nikeName 
+ * @param {*} portrait 
+ */
+const loginSuccess = function (resultMap, userId, nikeName, portrait) {
+  const payload = {
+    userId: userId
+  }
+  // 签发 Token
+  const token = jwt.sign(payload, secret, { expiresIn: '1day' })
+  // 输出签发的 Token
+  resultMap[constants.CODE] = constants.SUCCESS_CODE;
+  resultMap[constants.TOKEN] = token
+  resultMap[constants.DATA] = {
+    nike_name: nikeName,
+    portrait: portrait
+  }
+  return resultMap;
+}
+
 module.exports = {
   insertUsers: function (req, res, next) {
     let resultMap = {};
     pool.getConnection(function (err, connection) {
       //查询此邮箱是否存在
-      let selectOne = `select count(*) as count from (select 1 from users where email = ?) a`;
+      let selectOne = `select id,password from users where email = ?`;
       selectOne = mysql.format(selectOne, [req.body.email]);
       log.info(selectOne);
       connection.query(selectOne, function (err, rows, result) {
@@ -24,11 +47,42 @@ module.exports = {
           log.error(err);
           res.json(resultMap);
         } else {
-          if (rows[0].count > 0) {
-            resultMap[constants.CODE] = constants.FAIL_CODE;
-            resultMap[constants.MESSAGE] = constants.EMAILUSE;
-            res.json(resultMap);
+          if (rows[0]) {
+            //说明存在邮箱
+            if (!rows[0].password) {
+              let _date = new Date();
+              let userId = rows[0].id;
+              //说明是第三方登录注册的邮箱
+              //更新那条信息
+              let params = [
+                req.body.email,
+                req.body.password,
+                _date,
+                _date,
+                config.defaultImg,
+                userId
+              ];
+              let _sql = `update users set nike_name = ?,password=?,create_time=?,login_time=?,portrait=? where id = ?`;
+              _sql = mysql.format(_sql, params);
+              log.info(_sql);
+              connection.query(_sql, function (err, rows, result) {
+                if (err) {
+                  resultMap[constants.CODE] = constants.FAIL_CODE;
+                  resultMap[constants.MESSAGE] = constants.SYSTEM_ERROR;
+                  log.error(err);
+                } else {
+                  resultMap = loginSuccess(resultMap, userId, req.body.email, config.QNdomain + config.defaultImg);
+                }
+                res.json(resultMap);
+              });
+            } else {
+              //邮箱已被注册
+              resultMap[constants.CODE] = constants.FAIL_CODE;
+              resultMap[constants.MESSAGE] = constants.EMAILUSE;
+              res.json(resultMap);
+            }
           } else {
+            //没有邮箱 注册
             let _date = new Date();
             let params = [
               req.body.email,
@@ -44,23 +98,9 @@ module.exports = {
                 resultMap[constants.MESSAGE] = constants.SYSTEM_ERROR;
                 log.error(err);
               } else {
-                let insertId = rows.insertId;//获取自动生成的id
-                // Token 数据
-                const payload = {
-                  userId: insertId
-                }
-                // 签发 Token
-                const token = jwt.sign(payload, secret, { expiresIn: '1day' })
-                // 输出签发的 Token
-                resultMap[constants.CODE] = constants.SUCCESS_CODE;
-                resultMap[constants.TOKEN] = token
-                resultMap[constants.DATA] = {
-                  nike_name: req.body.email,
-                  portrait: config.QNdomain + config.defaultImg
-                }
+                resultMap = loginSuccess(resultMap, rows.insertId, req.body.email, config.QNdomain + config.defaultImg);
               }
               res.json(resultMap);
-              // connection.release();
             });
           }
         }
@@ -87,23 +127,37 @@ module.exports = {
           res.json(resultMap);
         } else {
           if (rows[0] && rows[0].id) {
+            //更新用户的第三方信息
+            let updatesql = `update open_users set portrait = ?,nike_name=? where open_id = ?`;
+            let params = [
+              req.portrait,
+              req.nikeName,
+              req.openId
+            ];
+            updatesql = mysql.format(updatesql, params);
+            log.info(updatesql);
+            connection.query(updatesql, function (err, rows, result) {
+              if (err) {
+                resultMap[constants.CODE] = constants.FAIL_CODE;
+                resultMap[constants.MESSAGE] = constants.SYSTEM_ERROR;
+                log.error(err);
+                res.json(resultMap);
+              }
+            });
             //授权表存在用户
             let userId = rows[0].user_id;
             if (userId) {
               //说明绑定了邮箱
               // 签发 Token
-              const payload = {
-                userId: userId
-              }
-              const token = jwt.sign(payload, secret, { expiresIn: '1day' })
-              let path = `${config.appServerIp}/page/oauthCallback?${constants.TOKEN}=${token}&nike_name=${req.nikeName}&portrait=${req.portrait}`;
-              //跳转到第三方授权回调页，并将token数据返回 保存到localstage中
-              res.redirect(path);
+              resultMap = loginSuccess(resultMap, userId, req.nikeName, req.portrait);
             } else {
-              let path = `${config.appServerIp}/page/bindEmail?openId=${req.openId}`;
-              //跳转到绑定邮箱的页面
-              res.redirect(path);
+              // let path = `${config.appServerIp}/page/bindEmail?openId=${req.openId}`;
+              // //跳转到绑定邮箱的页面
+              // res.redirect(path);
+              resultMap['openId'] = req.openId
             }
+            resultMap[constants.CODE] = constants.SUCCESS_CODE;
+            res.json(resultMap);
           } else {
             //不存在授权
             //新增
@@ -123,9 +177,12 @@ module.exports = {
                 log.error(err);
                 res.json(resultMap);
               } else {
-                let path = `${config.appServerIp}/page/bindEmail?openId=${req.openId}`;
-                //跳转到绑定邮箱的页面
-                res.redirect(path);
+                // let path = `${config.appServerIp}/page/bindEmail?openId=${req.openId}`;
+                // //跳转到绑定邮箱的页面
+                // res.redirect(path);
+                resultMap['openId'] = req.openId;
+                resultMap[constants.CODE] = constants.SUCCESS_CODE;
+                res.json(resultMap);
               }
             });
           }
@@ -148,25 +205,99 @@ module.exports = {
           log.error(err);
         } else {
           if (rows.length > 0) {
-            const payload = {
-              nikeName: rows[0].nike_name,
-              userId: rows[0].id
-            }
-            // 签发 Token
-            const token = jwt.sign(payload, secret, { expiresIn: '1day' })
-            // 输出签发的 Token
-            resultMap[constants.CODE] = constants.SUCCESS_CODE;
-            resultMap[constants.TOKEN] = token;
-            resultMap[constants.DATA] = {
-              nike_name: rows[0].nike_name,
-              portrait: config.QNdomain + rows[0].portrait
-            }
+            resultMap = loginSuccess(resultMap, rows[0].id, rows[0].nike_name, config.QNdomain + rows[0].portrait);
           } else {
             resultMap[constants.CODE] = constants.FAIL_CODE;
             resultMap[constants.MESSAGE] = constants.NOUSER;
           }
         }
         res.json(resultMap);
+        connection.release();
+      });
+    });
+  },
+  bindEmail: function (req, res, next) {
+    pool.getConnection(function (err, connection) {
+      //先查询邮箱是否注册
+      let params = [
+        req.body.email,
+        req.body.openId
+      ]
+      let _sql = `select nike_name,portrait,(select id from users where email = ?) as userId 
+        from open_users where open_id = ?`;
+      _sql = mysql.format(_sql, params);
+      log.info(_sql);
+      connection.query(_sql, function (err, rows, result) {
+        var resultMap = {};
+        if (err) {
+          resultMap[constants.CODE] = constants.FAIL_CODE;
+          resultMap[constants.MESSAGE] = constants.SYSTEM_ERROR;
+          log.error(err);
+          res.json(resultMap);
+        } else {
+          if (rows.length > 0) {
+            //邮箱存在
+            //更新第三方登录表的userid
+            let userInfo = rows[0];
+            if (rows[0].userId) {
+              let params = [
+                userInfo.userId,
+                req.body.openId
+              ]
+              let _sql = `update open_users set user_id = ? where open_id =?`;
+              _sql = mysql.format(_sql, params);
+              log.info(_sql);
+              connection.query(_sql, function (err, rows, result) {
+                if (err) {
+                  resultMap[constants.CODE] = constants.FAIL_CODE;
+                  resultMap[constants.MESSAGE] = constants.SYSTEM_ERROR;
+                  log.error(err);
+                } else {
+                  resultMap = loginSuccess(resultMap, userInfo.userId, userInfo.nike_name, userInfo.portrait);
+                  res.json(resultMap);
+                }
+              });
+            } else {
+              //邮箱不存在 
+              //在主表中插入一天模拟注册数据，密码为空表示不是正常注册而是第三方绑定数据
+              let params = [
+                req.body.email   //邮箱
+              ]
+              let _sql = `insert into users set email = ?`;
+              _sql = mysql.format(_sql, params);
+              log.info(_sql);
+              connection.query(_sql, function (err, rows, result) {
+                if (err) {
+                  resultMap[constants.CODE] = constants.FAIL_CODE;
+                  resultMap[constants.MESSAGE] = constants.SYSTEM_ERROR;
+                  log.error(err);
+                } else {
+                  let params = [
+                    rows.insertId,
+                    req.body.openId
+                  ]
+                  let _sql = `update open_users set user_id = ? where open_id =?`;
+                  _sql = mysql.format(_sql, params);
+                  log.info(_sql);
+                  connection.query(_sql, function (err, rows, result) {
+                    if (err) {
+                      resultMap[constants.CODE] = constants.FAIL_CODE;
+                      resultMap[constants.MESSAGE] = constants.SYSTEM_ERROR;
+                      log.error(err);
+                    }
+                  });
+                  //更新第三方登录表的userid
+                  resultMap = loginSuccess(resultMap, rows.insertId, userInfo.nike_name, userInfo.portrait);
+                  res.json(resultMap);
+                }
+              });
+            }
+          } else {
+            resultMap[constants.CODE] = constants.FAIL_CODE;
+            resultMap[constants.MESSAGE] = constants.NOUSER;
+            res.json(resultMap);
+          }
+        }
         connection.release();
       });
     });
